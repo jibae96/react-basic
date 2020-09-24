5ab5e9b6e6ca457c0b760a96b62443751a9f04ef
jungin <jibae96@naver.com>
2020-09-24 15:58:00 +0900

    로그아웃 기능 추가

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post('/api/users/login', (req, res) => {
         // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
         user.comparePassword(req.body.password, (err, isMatch) => {
             if(!isMatch)
-                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."});
 
             // 비밀번호까지 맞다면 토큰을 생성한다
             user.generateToken((err, user) => {
@@ -84,6 +84,20 @@ app.get('/api/users/auth', auth , (req,res) => {
         image: req.user.image
     })
 })
+
+app.get('/api/users/logout', auth, (req, res) => {
+    // console.log('req.user', req.user)
+    User.findOneAndUpdate({ _id: req.user._id },
+      { token: "" }
+      , (err, user) => {
+        if (err){
+            return res.json({ success: false, err });
+        } 
+        return res.status(200).send({
+          success: true
+        })
+      })
+  })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
