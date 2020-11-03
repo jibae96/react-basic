8d36b652c3263a04ba96d624de25fb0c445254b6
jungin <jibae96@naver.com>
2020-11-03 20:21:46 +0900

    인증 체크

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
 function LandingPage(props) {
 
@@ -23,15 +24,15 @@ function LandingPage(props) {
     return (
         <div style={{
             display:'flex', justifyContent:'center', alignItems:'center',
-            width:'100%', height:'100vh'
+            width:'100%', height:'100vh', flexDirection:'column'
         }}>
             <h2>시작 페이지</h2>
-
             <button onClick={onClickHandler}>
                 로그아웃
             </button>
+        
         </div>
     );
 };
 
-export default LandingPage;
+export default withRouter(LandingPage);
---
+++ client/src/hoc/auth.js
@@ -0,0 +1,50 @@
+import React, {userEffect, useEffect} from 'react';
+import Axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { auth } from '../_actions/user_action';
+
+export default function (SpecificComponent, option, adminRoute = null){
+
+    // option
+    // null = 아무나 출입이 가능한 페이지
+    // true = 로그인한 유저만 출입이 가능한 페이지
+    // false = 로그인한 유저는 출입 불가능한 페이지
+
+    // adminRoute 인자 안 쓰면 null임
+    // admin user만 들어가기 원하면 true
+
+    function AuthenticationCheck(props){
+
+        const dispatch = useDispatch();
+
+        useEffect(() => {
+                
+            dispatch(auth()).then(response => {
+                console.log(response)
+
+                // 로그인하지 않은 상태
+                if(!response.payload.isAuth){
+                    if(option){
+                        props.history.push('/login')
+                    }
+                }else{
+                    // 로그인한 상태
+                    if(adminRoute && !response.payload.isAdmin){
+                        props.history.push('/')
+                    }else{
+                        // option이 false일때
+                        if(option===false)
+                            props.history.push('/')
+                    }
+                }
+            })
+
+        }, [])
+
+        return (
+            <SpecificComponent />
+        )
+    }
+
+    return AuthenticationCheck;
+}
